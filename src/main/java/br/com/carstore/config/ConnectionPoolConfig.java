14ef062ceeccf9409db2436be6c29871fb778b7f
Eriklps <erikdslopes@gmail.com>
2023-10-25 14:31:34 -0300

    Created ConnectionPoolConfig

---
+++ src/main/java/br/com/carstore/config/ConnectionPoolConfig.java
@@ -0,0 +1,29 @@
+package br.com.carstore.config;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+public class ConnectionPoolConfig {
+
+    private static BasicDataSource dataSource;
+
+    public static BasicDataSource getDataSource() {
+
+        if (dataSource == null) {
+
+            dataSource = new BasicDataSource();
+            dataSource.setUrl("jdbc:h2:~/test");
+            dataSource.setUsername("sa");
+            dataSource.setPassword("sa");
+            dataSource.setMinIdle(5);
+            dataSource.setMaxIdle(10);
+            dataSource.setMaxTotal(50);
+
+            System.out.println("New connection pool successful created");
+
+        }
+
+        return dataSource;
+
+    }
+
+}
